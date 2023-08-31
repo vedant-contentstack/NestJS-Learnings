e7105702ca0832e418806a6e17d9f4a23f9317c2
vedant-contentstack <vedant.karle@contentstack.com>
2023-08-31 12:02:15 +0530

    added typeorm and sqlite

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
+import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule, ReportsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: [],
+      synchronize: true,
+    }),
+    ReportsModule,
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
