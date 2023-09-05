b7d49e605285acbcc5501360a173b8f9807e2c49
vedant-contentstack <vedant.karle@contentstack.com>
2023-09-05 10:57:02 +0530

    created a simple auth service test

---
+++ src/users/auth.service.spec.ts
@@ -0,0 +1,29 @@
+import { Test } from '@nestjs/testing';
+import { AuthService } from './auth.service';
+import { UsersService } from './users.service';
+
+describe('AuthService', () => {
+  let service: AuthService;
+
+  it('can create an instance of auth service', async () => {
+    const fakeUsersService: Partial<UsersService> = {
+      find: () => Promise.resolve([]),
+      create: (email: string, password: string) =>
+        Promise.resolve({ id: 1, email, password }),
+    };
+
+    const module = await Test.createTestingModule({
+      providers: [
+        AuthService,
+        {
+          provide: UsersService,
+          useValue: fakeUsersService,
+        },
+      ],
+    }).compile();
+
+    service = module.get(AuthService);
+
+    expect(service).toBeDefined();
+  });
+});
